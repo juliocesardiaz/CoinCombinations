1c4a29c8a80c8b60b5d83f354bde8afc824bfbeb
Chris Lytsell & Julio Diaz <student@epicodus.com>
2015-08-11 15:30:33 -0700

    get test to work

--- src/CoinCombination.php
+++ src/CoinCombination.php
@@ -3,6 +3,20 @@
 	{
 		function makeChange($amount)
 		{
+			$quarters = 0;
+			$dimes = 0;
+			$nickels = 0;
+			$pennies = 0;
+			
+			$quarters = (int) ($amount/25);
+			$amount = $amount % 25;
+			$dimes = (int) ($amount/10);
+			$amount = $amount % 10;
+			$nickels = (int) ($amount/5);
+			$amount = $amount % 5;
+			$pennies = (int) ($amount/1);
+			
+			return "Quarters: " . $quarters . " Dimes: " . $dimes . " Nickels: " . $nickels . " Pennies: " . $pennies;
 			
 		}
 	}	
